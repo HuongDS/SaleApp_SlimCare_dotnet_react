222b402d56523749251538c0b2385f463ea69f1a
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-08-08 22:52:39 +0700

    Implement unit-test-01

--- backend/Slimcare/SlimcareWeb.Service/Services/UserService.cs
+++ backend/Slimcare/SlimcareWeb.Service/Services/UserService.cs
@@ -16,11 +16,13 @@ namespace SlimcareWeb.Service.Services
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
+        private readonly BCryptHelper _BCryptHelper;
 
         public UserService(IUserRepository userRepository, IMapper mapper)
         {
             this._userRepository = userRepository;
             this._mapper = mapper;
+            this._BCryptHelper = new BCryptHelper();
         }
         public async Task<IEnumerable<User>> GetAllAsync()
         {
@@ -42,7 +44,7 @@ namespace SlimcareWeb.Service.Services
                 throw new Exception("Invalid username.");
             }
             data.Password += user.Salting;
-            if (!BCryptHelper.BCryptVerify(user.Password, data.Password))
+            if (!_BCryptHelper.BCryptVerify(data.Password, user.Password))
             {
                 throw new Exception("Password is not correct.");
             }
@@ -61,11 +63,11 @@ namespace SlimcareWeb.Service.Services
                 throw new Exception("Username already exists");
             }
             // Process password
-            var salt = BCryptHelper.GenerateSalting(100);
+            var salt = _BCryptHelper.GenerateSalting(100);
             data.Password += salt;
             var user = _mapper.Map<User>(data);
             user.Salting = salt;
-            user.Password = BCryptHelper.BCryptHash(user.Password);
+            user.Password = _BCryptHelper.BCryptHash(user.Password);
             user.Delete_At = DateTime.MinValue;
             await _userRepository.AddAsync(user);
             return user;
