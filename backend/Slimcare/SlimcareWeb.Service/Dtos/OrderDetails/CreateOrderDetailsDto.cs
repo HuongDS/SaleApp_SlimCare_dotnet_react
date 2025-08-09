7f95732f2e7f4821563a0eb9d73e394613d1ff4b
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-08-09 19:47:08 +0700

    Update model-validation

--- backend/Slimcare/SlimcareWeb.Service/Dtos/OrderDetails/CreateOrderDetailsDto.cs
+++ backend/Slimcare/SlimcareWeb.Service/Dtos/OrderDetails/CreateOrderDetailsDto.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -11,7 +12,9 @@
     {
         public int OrderId { get; set; }
         public int ProductId { get; set; }
+        [Range(0, Int32.MaxValue, ErrorMessage = "This field can not be negative.")]
         public int Quantity { get; set; }
+        [Range(0.0, Double.MaxValue, ErrorMessage = "This field can not be negative.")]
         public double Total_Price { get; set; }
     }
 }
