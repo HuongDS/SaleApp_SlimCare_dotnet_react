cc61b02c2c8975dfdccd3c0a73bf96108d4ab6df
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-08-22 01:10:38 +0700

    Update front login

---
+++ frontend/SlimcareApp/src/token/tokenStore.ts
@@ -0,0 +1,31 @@
+const ACCESS_KEY = import.meta.env.ACCESS_KEY;
+const REFRESH_KEY = import.meta.env.REFRESH_KEY;
+
+export function setTokens(
+  accessToken: string,
+  refreshToken?: string
+) {
+  sessionStorage.setItem(ACCESS_KEY, accessToken);
+  if (refreshToken)
+    localStorage.setItem(
+      REFRESH_KEY,
+      refreshToken
+    );
+}
+
+export function getAccessToken(): string | null {
+  return sessionStorage.getItem(ACCESS_KEY);
+}
+
+export function getRefreshToken(): string | null {
+  return localStorage.getItem(REFRESH_KEY);
+}
+
+export function clearTokens() {
+  sessionStorage.removeItem(ACCESS_KEY);
+  localStorage.removeItem(REFRESH_KEY);
+}
+
+export function isLoggedIn() {
+  return !!getAccessToken();
+}
